afda1076468a7dbe02489af24b7ff47478bcf886
Amr Sadat <amrsadat2017@gmail.com>
2025-08-25 19:06:07 +0300

    Create home component Adjust header resposive design

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from "../shared/header/header";
+
+@Component({
+  selector: 'app-home',
+  imports: [Header],
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss']
+})
+export class Home {
+
+}
